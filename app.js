a8c491b2f06037e1373f1cb2093438781b999775
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-13 14:50:49 +0600

    added home.ejs file and style added to that file

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express= require('express');
 const app = express();
+
+//set up view engine
+app.set('view engine','ejs');
+
+//create home route
+app.get('/',(req,res)=>{
+    res.render("home");
+})
+
+app.listen(3000,()=>{
+    console.log("app listening in port 3000")
+});
