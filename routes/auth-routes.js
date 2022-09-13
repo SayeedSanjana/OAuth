cb3a4196d63726672aeab01de0678d198db56afc
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-13 16:45:25 +0600

    routes added

---
+++ routes/auth-routes.js
@@ -0,0 +1,20 @@
+//create instance of the express router
+const router= require('express').Router();
+
+//auth login
+router.get('/login' ,(req,res)=>{
+    res.render('login');
+
+});
+
+//auth logout
+router.get('/logout',(req,res)=>{
+    //handle with passport
+    res.send("Logging out");
+})
+
+//auth with google
+router.get('/google',(req,res)=>{
+    //handle with passport
+    res.send("logging with google");
+})
