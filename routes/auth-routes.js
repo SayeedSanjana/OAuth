fb7b14ca51ebec45c3aa2ead6fef321c8605bd99
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-14 10:28:47 +0600

    login file added

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,17 +4,18 @@ const router= require('express').Router();
 //auth login
 router.get('/login' ,(req,res)=>{
     res.render('login');
-
 });
 
 //auth logout
 router.get('/logout',(req,res)=>{
     //handle with passport
     res.send("Logging out");
-})
+});
 
 //auth with google
 router.get('/google',(req,res)=>{
     //handle with passport
     res.send("logging with google");
-})
+});
+
+module.exports=router;
