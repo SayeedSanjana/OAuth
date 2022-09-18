7bd240839915f82cd3bbe154e00ba56b81806a5c
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-18 17:20:11 +0600

    added clientID and clientSecret

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,14 @@
 const passport= require('passport');
 const GoogleStrategy= require('passport-google-oauth20');
+const keys=require('./keys');
 
 
 //passport.use will take two parametres one is the Strategy and another is a callback function
 passport.use(new GoogleStartegy({
 //options for the google start
+clientID:keys.google.clientID,
+clientSecret:keys.google.clientSecret,
+
 }),()=>{
     //passport callback function
 })
