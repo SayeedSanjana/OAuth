5be9fad787e60368fb5819b96ba8a190e4968369
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-18 16:50:47 +0600

    passport installed

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,1 +1,10 @@
 const passport= require('passport');
+const GoogleStrategy= require('passport-google-oauth20');
+
+
+//passport.use will take two parametres one is the Strategy and another is a callback function
+passport.use(new GoogleStartegy({
+//options for the google start
+}),()=>{
+    //passport callback function
+})
