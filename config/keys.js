d013168b24137637aaddc686f4b243f560089461
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-19 09:57:13 +0600

    keys.js file removed from config

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-//add this file to gitignore
-
-module.exports={
-    google:{
-        clientID:'779136152697-k1u5jk6h6tv0a53pn29sudf1t5nihmjn.apps.googleusercontent.com',
-        clientSecret:'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'
-    }
-}
