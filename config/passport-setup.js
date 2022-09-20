f673b7a5839abc5c0772f7d7c8add53b28a9ffe5
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-20 16:48:50 +0600

    profile information of user retrieved from the google and then saved in the database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport= require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys=require('./keys');
+const User = require('../models/user-model');
 
 
 //passport.use will take two parametres one is the Strategy and another is a callback function
@@ -12,6 +13,25 @@ clientSecret:keys.google.clientSecret,
 
 },(accessToken,refreshToken,profile,done)=>{
     //passport callback function
-    console.log(profile);
+   // console.log(profile);
+
+   //check if user already exist in the database
+   User.findOne({googleId:profile.id}).then((currentUser)=>{
+    if(currentUser){
+        //already have the user
+        console.log("user is: ",currentUser)
+
+    }else{
+        //if not create user to the database
+        new User({
+            googleId: profile.id,
+            username: profile.displayName
+        }).save().then((newUser) => {
+            console.log('new user created: ', newUser);
+        });
+
+    }
+   });
+    
 })
-)
+);
