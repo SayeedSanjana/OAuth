f5106b5a5b18692105fea39cae9fc4e2ba3fbaa5
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2022-09-21 15:23:49 +0600

    cookie-session initialized

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const passportSetup=require('./config/passport-setup');
 const mongoose=require('mongoose');
 const keys=require('./config/keys');
 const app = express();
+const cookieSession=require('cookie-session');
+const passport= require('passport');
 
 
 //set up view engine
 app.set('view engine','ejs');
 
+app.use(cookieSession({
+    maxAge:24*60*60*1000,
+    keys:[keys.session.cookieKey]
+}))
+
+//initialize passport (imiddleware)
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, ()=>{
     console.log("Connected to mongodb");
 })
+
 //set routes
 app.use('/auth',authRoutes);
 
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,37 +1,42 @@
-const passport= require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys=require('./keys');
+const keys = require('./keys');
 const User = require('../models/user-model');
 
 
-//passport.use will take two parametres one is the Strategy and another is a callback function
-passport.use(new GoogleStrategy({
-//options for the google start
-callbackURL:'/auth/google/redirect',
-clientID:keys.google.clientID,
-clientSecret:keys.google.clientSecret,
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-},(accessToken,refreshToken,profile,done)=>{
-    //passport callback function
-   // console.log(profile);
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
-   //check if user already exist in the database
-   User.findOne({googleId:profile.id}).then((currentUser)=>{
-    if(currentUser){
-        //already have the user
-        console.log("user is: ",currentUser)
-
-    }else{
-        //if not create user to the database
-        new User({
-            googleId: profile.id,
-            username: profile.displayName
-        }).save().then((newUser) => {
-            console.log('new user created: ', newUser);
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
         });
-
-    }
-   });
-    
-})
+    })
 );
